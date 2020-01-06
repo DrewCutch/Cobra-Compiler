4c28509d07c908a59318842d77c28eb897a4163c
Drew Cutchins <drew.cutch@gmail.com>
2020-01-05 23:29:50 -0500

    Add WhileStatement model

---
+++ CobraCompiler/Parse/Statements/WhileStatement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Expressions;
+
+namespace CobraCompiler.Parse.Statements
+{
+    class WhileStatement: Statement
+    {
+        public readonly Expression Condition;
+        public readonly Statement Body;
+        public readonly Statement Else;
+
+        public WhileStatement(Expression condition, Statement body, Statement @else)
+        {
+            Condition = condition;
+            Body = body;
+            Else = @else;
+        }
+    }
+}
