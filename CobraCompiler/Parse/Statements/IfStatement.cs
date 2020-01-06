4ac0a6d3e49a96919d7d1340a1057d105ecae0b3
Drew Cutchins <drew.cutch@gmail.com>
2020-01-05 22:29:08 -0500

    Add IfStatement model

---
+++ CobraCompiler/Parse/Statements/IfStatement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Expressions;
+
+namespace CobraCompiler.Parse.Statements
+{
+    class IfStatement: Statement
+    {
+        public readonly Expression Condition;
+        public readonly Statement Then;
+        public readonly Statement Else;
+
+        public IfStatement(Expression condition, Statement then, Statement @else)
+        {
+            Condition = condition;
+            Then = then;
+            Else = @else;
+        }
+
+    }
+}
