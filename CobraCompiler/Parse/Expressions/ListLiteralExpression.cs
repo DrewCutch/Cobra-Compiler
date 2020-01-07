6be059d981789e52cf660b2369ba5513670bb182
Drew Cutchins <drew.cutch@gmail.com>
2020-01-07 18:26:30 -0500

    Add ListLiteralExpression model

---
+++ CobraCompiler/Parse/Expressions/ListLiteralExpression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Parse.Expressions
+{
+    class ListLiteralExpression: Expression
+    {
+        public readonly IReadOnlyList<Expression> Elements;
+
+        public ListLiteralExpression(IEnumerable<Expression> elements)
+        {
+            Elements = new List<Expression>(elements);
+        }
+
+        public override T Accept<T>(IExpressionVisitor<T> expressionVisitor)
+        {
+            return expressionVisitor.Visit(this);
+        }
+
+        public override void Accept<T>(IExpressionTraverser<T> expressionTraverser, T arg)
+        {
+            expressionTraverser.Visit(this, arg);
+        }
+
+        public override T Accept<T, TU>(IExpressionVisitorWithContext<T, TU> expressionVisitor, TU arg)
+        {
+            return expressionVisitor.Visit(this, arg);
+        }
+    }
+}
