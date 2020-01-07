ce06abb340bfe0f5356100084492c925f0ab0d00
Drew Cutchins <drew.cutch@gmail.com>
2020-01-07 16:44:26 -0500

    Add ParentExpressionAssemblyContext model

---
+++ CobraCompiler/Assemble/ExpressionAssemblyContexts/ParentExpressionAssemblyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Assemble.ExpressionAssemblyContexts
+{
+    class ParentExpressionAssemblyContext
+    {
+        public readonly bool ImmediatelyCalling;
+
+        public ParentExpressionAssemblyContext(bool immediatelyCalling)
+        {
+            ImmediatelyCalling = immediatelyCalling;
+        }
+    }
+}
