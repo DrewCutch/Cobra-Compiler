a1b7cfd1036b9cda40a72aadbe86cc72cfa11bcd
Drew Cutchins <drew.cutch@gmail.com>
2020-01-07 16:29:37 -0500

    Update using statement for new TypeCheck.Types namespace

--- CobraCompiler/SymbolTables/Symbol.cs
+++ CobraCompiler/SymbolTables/Symbol.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CobraCompiler.Parse.TypeCheck;
+using CobraCompiler.Parse.TypeCheck.Types;
 
 namespace CobraCompiler.SymbolTables
 {
