ba2fb9f0d034a214f50e4b6933cb4809d962be83
Drew Cutchins <drew.cutch@gmail.com>
2020-01-07 18:26:10 -0500

    Add ability to get common base to Cobra Type

--- CobraCompiler/Parse/TypeCheck/Types/CobraType.cs
+++ CobraCompiler/Parse/TypeCheck/Types/CobraType.cs
@@ -1,4 +1,7 @@
-﻿namespace CobraCompiler.Parse.TypeCheck.Types
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CobraCompiler.Parse.TypeCheck.Types
 {
     abstract class CobraType
     {
@@ -12,6 +15,24 @@
         public virtual bool CanImplicitCast(CobraType other)
         {
             return this.Equals(other);
+        }
+
+        public virtual CobraType GetCommonParent(CobraType other)
+        {
+            if (Equals(other))
+                return this;
+
+            return DotNetCobraType.Object;
+        }
+
+        public static CobraType GetCommonParent(IEnumerable<CobraType> types)
+        {
+            CobraType commonType = types.First();
+
+            foreach (CobraType type in types)
+                commonType = commonType.GetCommonParent(type);
+
+            return commonType;
         }
     }
 }
