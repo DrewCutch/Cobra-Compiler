8f78eee8318b0edb46e420ed96010a3d307cbccd
Drew Cutchins <drew.cutch@gmail.com>
2020-01-07 18:30:49 -0500

    Add ListType to DotNetCobraGeneric

--- CobraCompiler/Parse/TypeCheck/Types/DotNetCobraGeneric.cs
+++ CobraCompiler/Parse/TypeCheck/Types/DotNetCobraGeneric.cs
@@ -10,10 +10,11 @@ namespace CobraCompiler.Parse.TypeCheck.Types
     class DotNetCobraGeneric: CobraGeneric
     {
         public static DotNetCobraGeneric FuncType = new DotNetCobraGeneric("f", -1, Expression.GetDelegateType);
-
+        public static DotNetCobraGeneric ListType = new DotNetCobraGeneric("list", 1, typeof(List<>).MakeGenericType);
         public static CobraGeneric[] BuiltInCobraGenerics =
         {
-            FuncType
+            FuncType,
+            ListType
         };
 
         public readonly GenericInstanceGenerator InstanceGenerator;
