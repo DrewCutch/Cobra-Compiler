3343e5aabaee1351f86b3f16850dfdd2b0a2a17d
Drew Cutchins <drew.cutch@gmail.com>
2020-01-07 18:25:40 -0500

    Add builtin object type to DotNetCobraType

--- CobraCompiler/Parse/TypeCheck/Types/DotNetCobraType.cs
+++ CobraCompiler/Parse/TypeCheck/Types/DotNetCobraType.cs
@@ -9,10 +9,11 @@ namespace CobraCompiler.Parse.TypeCheck.Types
         public static DotNetCobraType Float = new DotNetCobraType("float", typeof(float));
         public static DotNetCobraType Str = new DotNetCobraType("str", typeof(string));
         public static DotNetCobraType Null = new DotNetCobraType("null", null);
+        public static DotNetCobraType Object = new DotNetCobraType("obj", typeof(object));
 
         public static DotNetCobraType[] DotNetCobraTypes =
         {
-            Bool, Int, Float, Str, Null
+            Bool, Int, Float, Str, Null, Object
         };
 
         public Type Type { get; }
