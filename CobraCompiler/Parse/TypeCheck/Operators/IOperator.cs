e072862e6df2b5d626465b40bc313712e64027f4
Drew Cutchins <drew.cutch@gmail.com>
2020-01-08 17:03:19 -0500

    Rename Operator file

--- CobraCompiler/Parse/TypeCheck/Operators/IOperator.cs
+++ CobraCompiler/Parse/TypeCheck/Operators/IOperator.cs
@@ -1,9 +1,10 @@
-﻿using CobraCompiler.Parse.TypeCheck.Types;
+﻿using System;
+using CobraCompiler.Parse.TypeCheck.Types;
 
 namespace CobraCompiler.Parse.TypeCheck.Operators
 {
     interface IOperator
-    {
+    { 
         TokenType OperatorToken { get; }
         CobraType ResultType { get; }
     }
