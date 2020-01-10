3a4d629a78cd5e73b6382f70a864f1337cda548f
Drew Cutchins <drew.cutch@gmail.com>
2020-01-09 21:40:30 -0500

    Add Operation enum

---
+++ CobraCompiler/Parse/TypeCheck/Operators/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Parse.TypeCheck.Operators
+{
+    enum Operation
+    {
+        Add,
+        CompareEqual,
+        CompareGreater,
+        CompareGreaterEqual,
+        CompareLess,
+        CompareLessEqual,
+        CompareNotEqual,
+        Devide,
+        Get,
+        Multiply,
+        Subtract
+    }
+}
