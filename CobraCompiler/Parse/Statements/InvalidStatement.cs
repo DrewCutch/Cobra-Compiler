635d495d353700748ecc20853b3f6f8729f3ce36
Drew Cutchins <drew.cutch@gmail.com>
2020-02-12 17:14:34 -0800

    Add InvalidStatement model

---
+++ CobraCompiler/Parse/Statements/InvalidStatement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Parse.Statements
+{
+    class InvalidStatement : Statement
+    {
+        public InvalidStatement()
+        {
+        }
+    }
+}
