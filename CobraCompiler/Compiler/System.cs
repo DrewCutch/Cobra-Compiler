4d7b7bbe2a1d9892f07e689a1f27556a6e69c2d4
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:55:53 -0400

    Create System model

---
+++ CobraCompiler/Compiler/System.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Compiler
+{
+    class System
+    {
+        public const string SystemPathDelimiter = ".";
+
+        public readonly string Name;
+        public readonly string SystemPath;
+        public string FullName => SystemPath + SystemPathDelimiter + Name;
+        public readonly IReadOnlyList<System> SubSystems;
+        public readonly IReadOnlyList<Module> Modules;
+
+        public System(string name, string systemPath, IEnumerable<System> subSystems, IEnumerable<Module> modules)
+        {
+            Name = name;
+            SystemPath = systemPath;
+            SubSystems = new List<System>(subSystems);
+            Modules = new List<Module>(modules);
+        }
+
+        public System(string name, string systemPath, IEnumerable<Module> modules)
+        {
+            Name = name;
+            SystemPath = systemPath;
+            Modules = new List<Module>(modules);
+            SubSystems = new List<System>();
+        }
+
+        public System(string name, string systemPath)
+        {
+            Name = name;
+            SystemPath = systemPath;
+            Modules = new List<Module>();
+            SubSystems = new List<System>();
+        }
+    }
+}
