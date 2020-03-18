2238836a571e2835cd577cf2358eb9e3a7ba67d0
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:56:10 -0400

    Create CompilerException

---
+++ CobraCompiler/Compiler/CompilerException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Compiler
+{
+    class CompilerException : Exception
+    {
+        public CompilerException(string message) : base(message)
+        {
+        }
+    }
+}
