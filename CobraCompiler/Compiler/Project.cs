e2693f54dc3a2454fc7c2177f65af943e57397f9
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:55:27 -0400

    Create Project Model

---
+++ CobraCompiler/Compiler/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Compiler
+{
+    class Project
+    {
+        public readonly string Name;
+        public readonly IReadOnlyList<System> Systems;
+
+        public Project(string name, IEnumerable<System> systems)
+        {
+            Name = name;
+            Systems = new List<System>(systems);
+        }
+    }
+}
