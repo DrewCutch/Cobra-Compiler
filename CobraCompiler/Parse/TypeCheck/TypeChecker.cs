25800dbd57966f2bf00a21112282a4057785ec12
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 16:18:41 -0400

    Add namespace handling to TypeChecker

--- CobraCompiler/Parse/TypeCheck/TypeChecker.cs
+++ CobraCompiler/Parse/TypeCheck/TypeChecker.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms.VisualStyles;
+using CobraCompiler.Compiler;
 using CobraCompiler.ErrorLogging;
 using CobraCompiler.Parse.Expressions;
 using CobraCompiler.Parse.Scopes;
 using CobraCompiler.Parse.Statements;
 using CobraCompiler.Parse.TypeCheck.Operators;
 using CobraCompiler.Parse.TypeCheck.Types;
+using System = CobraCompiler.Compiler.System;
 
 namespace CobraCompiler.Parse.TypeCheck
 {
@@ -15,7 +17,7 @@ namespace CobraCompiler.Parse.TypeCheck
     {
         private readonly ErrorLogger _errorLogger;
 
-        private readonly Scope _globalScope;
+        private readonly GlobalScope _globalScope;
 
         private readonly Queue<Scope> _scopes;
         private Scope CurrentScope => _scopes.Peek();
@@ -39,6 +41,52 @@ namespace CobraCompiler.Parse.TypeCheck
             _globalScope.Declare("printInt", DotNetCobraGeneric.FuncType.CreateGenericInstance(new[] { DotNetCobraType.Int, DotNetCobraType.Null }));
         }
 
+        public void DefineNamespaces(Project project)
+        {
+            Stack<Compiler.System> systems = new Stack<Compiler.System>();
+            NamespaceType globalNamespace = new NamespaceType(project.Name);
+
+            foreach (Compiler.System system in project.Systems)
+            {
+
+                NamespaceType systemNamespace = globalNamespace.AddSubNameSpace(system.Name);
+
+                foreach (Module module in system.Modules)
+                {
+                    systemNamespace.AddSubNameSpace(module.Name);
+                }
+            }
+
+            _globalScope.Declare(project.Name, globalNamespace);
+        }
+
+        public GlobalScope Check(IEnumerable<ParsedModule> modules)
+        {
+            List<ModuleScope> moduleScopes = new List<ModuleScope>();
+
+            foreach (ParsedModule module in modules)
+            {
+                BlockStatement baseBlock = new BlockStatement(module.Statements);
+                ModuleScope moduleScope = new ModuleScope(_globalScope, baseBlock, module.FullName);
+                
+                _globalScope.AddSubScope(moduleScope);
+                moduleScopes.Add(moduleScope);
+            }
+
+            foreach (ModuleScope scope in moduleScopes)
+            {
+                _scopes.Enqueue(scope);
+            }
+
+            while (_scopes.Count > 0)
+            {
+                CheckScope(CurrentScope);
+                _scopes.Dequeue();
+            }
+
+            return _globalScope;
+        } 
+
         public ModuleScope CreateModule(IReadOnlyList<Statement> statements, string moduleName)
         {
             BlockStatement baseBlock = new BlockStatement(statements);
@@ -53,11 +101,6 @@ namespace CobraCompiler.Parse.TypeCheck
             }
 
             return moduleScope;
-        }
-
-        public void Check(IReadOnlyList<Statement> statements, string moduleName)
-        {
-            CreateModule(statements, moduleName);
         }
 
         private void CheckScope(Scope scope)
@@ -93,7 +136,7 @@ namespace CobraCompiler.Parse.TypeCheck
 
                 FuncScope funcScope = new FuncScope(scope, funcDeclaration,
                     funcDeclaration.Params.Select(param => (param.Name.Lexeme, scope.GetType(param.TypeInit))),
-                    scope.GetType(funcDeclaration.ReturnType));
+                    returnType);
 
                 List<CobraType> typeArgs = funcDeclaration.Params.Select(param => scope.GetType(param.TypeInit)).ToList();
                 typeArgs.Add(funcScope.ReturnType);
@@ -134,53 +177,63 @@ namespace CobraCompiler.Parse.TypeCheck
 
             foreach (Statement statement in statements)
             {
-                if (statement is VarDeclarationStatement varDeclaration)
+                Check(statement);
+            }
+        }
+
+        private void Check(Statement statement)
+        {
+            try
+            {
+                switch (statement)
                 {
-                    if (!scope.IsTypeDefined(varDeclaration.TypeInit))
-                        _errorLogger.Log(new TypeNotDefinedException(varDeclaration.TypeInit.Identifier.First()));
+                    case VarDeclarationStatement varDeclaration:
+                        if (!CurrentScope.IsTypeDefined(varDeclaration.TypeInit))
+                            throw new TypeNotDefinedException(varDeclaration.TypeInit.Identifier.First());
 
-                    if (scope.IsDeclared(varDeclaration.Name.Lexeme))
-                        _errorLogger.Log(new VarAlreadyDeclaredException(varDeclaration.Name));
+                        if (CurrentScope.IsDeclared(varDeclaration.Name.Lexeme))
+                            throw new VarAlreadyDeclaredException(varDeclaration.Name);
 
-                    scope.Declare(varDeclaration.Name.Lexeme, varDeclaration.TypeInit);
-                    varDeclaration.Assignment?.Accept(this);
-                }
+                        CurrentScope.Declare(varDeclaration.Name.Lexeme, varDeclaration.TypeInit);
+                        varDeclaration.Assignment?.Accept(this);
+                        break;
+                    case ParamDeclarationStatement paramDeclaration:
+                        if (!CurrentScope.IsTypeDefined(paramDeclaration.TypeInit))
+                            throw new TypeNotDefinedException(paramDeclaration.TypeInit.Identifier.First());
 
-                if (statement is ParamDeclarationStatement paramDeclaration)
-                {
-                    if (!scope.IsTypeDefined(paramDeclaration.TypeInit))
-                        _errorLogger.Log(new TypeNotDefinedException(paramDeclaration.TypeInit.Identifier.First()));
+                        if (CurrentScope.IsDeclared(paramDeclaration.Name.Lexeme))
+                            throw new VarAlreadyDeclaredException(paramDeclaration.Name);
 
-                    if(scope.IsDeclared(paramDeclaration.Name.Lexeme))
-                        _errorLogger.Log(new VarAlreadyDeclaredException(paramDeclaration.Name));
-
-                    scope.Declare(paramDeclaration.Name.Lexeme, paramDeclaration.TypeInit);
-                }
-
-                if (statement is ExpressionStatement expressionStatement)
-                {
-                    expressionStatement.Expression.Accept(this);
-                }
-
-                if (statement is ReturnStatement returnStatement)
-                {
-                    CobraType returnStatementType = returnStatement.Value.Accept(this);
-                    if(returnStatementType != scope.GetReturnType())
-                        _errorLogger.Log(new InvalidReturnTypeException(returnStatement.Keyword, returnStatementType, scope.GetReturnType()));
+                        CurrentScope.Declare(paramDeclaration.Name.Lexeme, paramDeclaration.TypeInit);
+                        break;
+                    case ExpressionStatement expressionStatement:
+                        expressionStatement.Expression.Accept(this);
+                        break;
+                    case ReturnStatement returnStatement:
+                        CobraType returnStatementType = returnStatement.Value.Accept(this);
+                        if (returnStatementType != CurrentScope.GetReturnType())
+                            throw new InvalidReturnTypeException(returnStatement.Keyword,
+                                returnStatementType, CurrentScope.GetReturnType());
+                        break;
                 }
             }
+            catch (TypingException typingException)
+            {
+                _errorLogger.Log(typingException);
+            }
+            
         }
 
         public CobraType Visit(AssignExpression expr)
         {
             if(!CurrentScope.IsDefined(expr.Name.Lexeme))
-                _errorLogger.Log(new VarNotDefinedException(expr.Name.Lexeme, expr.Name.Line));
+                throw new VarNotDefinedException(expr.Name.Lexeme, expr.Name.Line);
 
             CobraType varType = CurrentScope.GetVarType(expr.Name.Lexeme);
             CobraType assignType = expr.Value.Accept(this);
 
             if (!varType.CanImplicitCast(assignType))
-                _errorLogger.Log(new InvalidAssignmentException(varType.Identifier, assignType?.Identifier, expr.Name.Line));
+                throw new InvalidAssignmentException(varType.Identifier, assignType?.Identifier, expr.Name.Line);
 
             return varType;
         }
@@ -192,8 +245,7 @@ namespace CobraCompiler.Parse.TypeCheck
 
             if (!CurrentScope.IsOperatorDefined(Operator.GetOperation(expr.Op.Type), leftType, rightType))
             {
-                _errorLogger.Log(new OperatorNotDefinedException(expr.Op, leftType, rightType));
-                return null;
+                throw new OperatorNotDefinedException(expr.Op, leftType, rightType);
             }
 
             IOperator op = CurrentScope.GetOperator(Operator.GetOperation(expr.Op.Type), leftType, rightType);
@@ -205,10 +257,25 @@ namespace CobraCompiler.Parse.TypeCheck
         {
             CobraType calleeType = expr.Callee.Accept(this);
             if (calleeType is CobraGenericInstance generic && generic.Base == DotNetCobraGeneric.FuncType)
-                return generic.TypeParams.Last();
+            {
+                if (generic.TypeParams.Count - 1 != expr.Arguments.Count)
+                {
+                    throw new IncorrectArgumentCountException(expr.Paren, expectedArgs: generic.TypeParams.Count - 1, providedArgs: expr.Arguments.Count);
+                }
 
-            _errorLogger.Log(new InvalidOperationException(expr.Paren.Line));
-            return null;
+                for (int i = 0; i < generic.TypeParams.Count - 1; i++)
+                {
+                    if (!generic.TypeParams[i].CanImplicitCast(expr.Arguments[i].Accept(this)))
+                    {
+                        CobraType test = expr.Arguments[i].Accept(this);
+                        throw new InvalidArgumentException(expr.Paren);
+                    }
+                }
+                return generic.TypeParams.Last();
+            }
+                
+
+            throw new InvalidOperationException(expr.Paren.Line);
         }
 
         public CobraType Visit(IndexExpression expr)
@@ -252,7 +319,7 @@ namespace CobraCompiler.Parse.TypeCheck
             CobraType operand = expr.Right.Accept(this);
 
             if (!CurrentScope.IsOperatorDefined(Operator.GetOperation(expr.Op.Type), null, operand))
-                _errorLogger.Log(new OperatorNotDefinedException(expr.Op, operand));
+                throw new OperatorNotDefinedException(expr.Op, operand);
 
             IOperator op = CurrentScope.GetOperator(Operator.GetOperation(expr.Op.Type), null, operand);
 
@@ -261,6 +328,11 @@ namespace CobraCompiler.Parse.TypeCheck
 
         public CobraType Visit(GetExpression expr)
         {
+            if (IsIdentifier(expr))
+            {
+                return CurrentScope.GetVarType(ToIdentifier(expr));
+            }
+
             throw new NotImplementedException();
         }
 
@@ -271,7 +343,32 @@ namespace CobraCompiler.Parse.TypeCheck
 
         public CobraType Visit(VarExpression expr)
         {
+            if(!CurrentScope.IsDefined(expr.Name.Lexeme))
+                throw new VarNotDefinedException(expr.Name.Lexeme, expr.Name.Line);
+
             return CurrentScope.GetVarType(expr.Name.Lexeme);
+        }
+
+        private string ToIdentifier(Expression expr)
+        {
+            if (expr is VarExpression var)
+                return var.Name.Lexeme;
+
+            if (expr is GetExpression get)
+                return ToIdentifier(get.Obj) + "." + get.Name.Lexeme;
+
+            throw new ArgumentException("Expression is not identifier!");
+        }
+
+        private bool IsIdentifier(Expression expr)
+        {
+            if (expr is VarExpression)
+                return true;
+
+            if (expr is GetExpression get)
+                return IsIdentifier(get.Obj);
+
+            return false;
         }
     }
 
