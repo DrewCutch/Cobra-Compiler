94dee8349fe2bddf3654710ef7d31b83b05dbd1b
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:57:33 -0400

    Create CheckedProject model

---
+++ CobraCompiler/Compiler/CheckedProject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Scopes;
+
+namespace CobraCompiler.Compiler
+{
+    class CheckedProject: Project
+    {
+        public readonly GlobalScope Scope;
+
+        public CheckedProject(Project proj, GlobalScope scope) : base(proj.Name, proj.Systems)
+        {
+            Scope = scope;
+        }
+    }
+}
