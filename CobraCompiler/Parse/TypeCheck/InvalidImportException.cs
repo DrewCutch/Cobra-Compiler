1f013c6312a14e6ab740e7f93a0f768437a1d83c
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 17:48:40 -0400

    Create InvalidImportException

---
+++ CobraCompiler/Parse/TypeCheck/InvalidImportException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Parse.TypeCheck
+{
+    class InvalidImportException : TypingException
+    {
+        public override bool isWarning => false;
+        public InvalidImportException(string type, int lineNumber) : base($"Cannot import {type}", lineNumber)
+        {
+
+        }
+    }
+}
