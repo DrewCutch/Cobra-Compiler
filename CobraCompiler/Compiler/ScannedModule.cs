7c5776964b3723e28e3051e79d6be5e5179867c1
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:56:57 -0400

    Create ScannedModule model

---
+++ CobraCompiler/Compiler/ScannedModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Compiler
+{
+    class ScannedModule: Module
+    {
+        public readonly IReadOnlyList<Token> Tokens;
+
+        public ScannedModule(Module module, IEnumerable<Token> tokens) : 
+            base(module.Name, module.SystemPath, module.File)
+        {
+            Tokens = new List<Token>(tokens);
+        }
+    }
+}
