54a3635da3fb1bbeb80829df6ad26a263582089e
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 16:13:47 -0400

    Create Compiler

--- CobraCompiler/Compiler/Compiler.cs
+++ CobraCompiler/Compiler/Compiler.cs
@@ -22,6 +22,106 @@ namespace CobraCompiler.Compiler
             ErrorLogger = errorLogger;
         }
 
+        public void Compile(Project project)
+        {
+            IReadOnlyList<ScannedModule> scannedModules = Scan(project);
+            IReadOnlyList<ParsedModule> parsedModules = Parse(scannedModules);
+            CheckedProject checkedProject = Check(project, parsedModules);
+            Assemble(checkedProject);
+        }
+
+        private List<ScannedModule> Scan(Project project)
+        {
+            Queue<System> systems = new Queue<System>(project.Systems);
+            List<ScannedModule> scannedModules = new List<ScannedModule>();
+
+            do
+            {
+                System next = systems.Dequeue();
+
+                foreach (Module module in next.Modules)
+                    scannedModules.Add(Scan(module));
+
+                foreach (System subSystem in next.SubSystems)
+                    systems.Enqueue(subSystem);
+            } while (systems.Count > 0);
+
+            return scannedModules;
+        }
+
+        private ScannedModule Scan(Module module)
+        {
+            Scanner scanner = new Scanner(module.File, ErrorLogger);
+            IReadOnlyList<Token> tokens = scanner.GetTokens();
+
+            if (Options.Flags.HasFlag(CompilerFlags.PrintScan))
+            {
+                foreach (Token token in tokens)
+                {
+                    if (token.Type == TokenType.NewLine)
+                        Console.WriteLine(token);
+                    else
+                        Console.Write(token);
+                }
+                Console.WriteLine();
+            }
+
+            if (ErrorLogger.HasErrors)
+                throw new CompilerException("Error scanning module!");
+
+
+            return new ScannedModule(module, tokens);
+        }
+
+        private List<ParsedModule> Parse(IEnumerable<ScannedModule> modules)
+        {
+            List<ParsedModule> parsedModules = new List<ParsedModule>();
+
+            foreach (ScannedModule module in modules)
+                parsedModules.Add(Parse(module));
+
+            return parsedModules;
+        }
+
+        private ParsedModule Parse(ScannedModule module)
+        {
+            Parser parser = new Parser(module.Tokens, ErrorLogger);
+            List<Statement> statements = parser.Parse();
+
+            if (Options.Flags.HasFlag(CompilerFlags.PrintParse))
+            {
+                AstPrinter astPrinter = new AstPrinter();
+                astPrinter.PrintStatements(statements);
+            }
+
+            if (ErrorLogger.HasErrors)
+                throw new CompilerException("Error parsing module!");
+
+            return new ParsedModule(module, statements);
+        }
+
+        private CheckedProject Check(Project project, IEnumerable<ParsedModule> parsedModules)
+        {
+            TypeChecker typeChecker = new TypeChecker(ErrorLogger);
+            typeChecker.DefineNamespaces(project);
+
+            if (ErrorLogger.HasErrors)
+                throw new CompilerException("Error checking project!");
+
+            return new CheckedProject(project, typeChecker.Check(parsedModules));
+        }
+
+        private void Assemble(CheckedProject project)
+        {
+            Assembler assembler = new Assembler(project.Name);
+            assembler.AssembleProject(project);
+
+            if (ErrorLogger.HasErrors)
+                throw new CompilerException("Error assembling!");
+
+            assembler.SaveAssembly();
+        }
+
         public void Run()
         {
             FileReader fileReader = new FileReader(Options.FilePath);
@@ -64,7 +164,7 @@ namespace CobraCompiler.Compiler
                 return;
 
             Assembler assembler = new Assembler("CobraTest");
-            assembler.AssembleModule(module);
+            //assembler.AssembleModule(module);
 
             if (ErrorLogger.HasErrors)
                 return;
