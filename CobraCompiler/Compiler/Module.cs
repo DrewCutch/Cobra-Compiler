f2ff74393779c6c34929a3aee1e369efd4c0b9a4
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:55:35 -0400

    Create Module model

---
+++ CobraCompiler/Compiler/Module.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Compiler
+{
+    class Module
+    {
+        public readonly string Name;
+        public readonly string SystemPath;
+        public string FullName => SystemPath + System.SystemPathDelimiter + Name;
+        public readonly FileReader File;
+
+        public Module(string name, string systemPath, FileReader file)
+        {
+            Name = name;
+            SystemPath = systemPath;
+            File = file;
+        }
+    }
+}
