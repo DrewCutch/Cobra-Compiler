319953504a7b7105d1be706b670b0c1c88667e96
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:53:42 -0400

    Create IncorrectArgumentCountException

---
+++ CobraCompiler/Parse/TypeCheck/IncorrectArgumentCountException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Parse.TypeCheck
+{
+    class IncorrectArgumentCountException: TypingException
+    {
+        public override bool isWarning => false;
+
+        public IncorrectArgumentCountException(Token token, int expectedArgs, int providedArgs) : base($"Function expects {expectedArgs} arguments but is provided {providedArgs}", token.Line)
+        {
+
+        }
+    }
+}
