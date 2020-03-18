019f6a5c42275686e0ab2ecf1eef6d7decbf94ac
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:57:08 -0400

    Create ParsedModule model

---
+++ CobraCompiler/Compiler/ParsedModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Statements;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Compiler
+{
+    class ParsedModule: ScannedModule
+    {
+        public readonly IReadOnlyList<Statement> Statements;
+
+        public ParsedModule(ScannedModule module, IEnumerable<Statement> statements) : base(module, module.Tokens)
+        {
+            Statements = new List<Statement>(statements);
+        }
+    }
+}
