cb66b6e48861b8b716a00eaeff6c3b666dc08a97
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 15:53:53 -0400

    Create InvalidArgumentException

---
+++ CobraCompiler/Parse/TypeCheck/InvalidArgumentException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Parse.TypeCheck
+{
+    class InvalidArgumentException: TypingException
+    {
+        public override bool isWarning => false;
+
+        public InvalidArgumentException(Token token) : base("Invalid argument", token.Line)
+        {
+        }
+    }
+}
