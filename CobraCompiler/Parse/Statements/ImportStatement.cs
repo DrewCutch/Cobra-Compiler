7490f3aef125f8cc20aa68b995c35cbb68c47cf6
Drew Cutchins <drew.cutch@gmail.com>
2020-03-18 17:48:30 -0400

    Create ImportStatement model

---
+++ CobraCompiler/Parse/Statements/ImportStatement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Expressions;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Parse.Statements
+{
+    class ImportStatement: Statement
+    {
+        public readonly Token Keyword;
+
+        public readonly Expression Import;
+
+        public ImportStatement(Token keyword, Expression import)
+        {
+            Keyword = keyword;
+            Import = import;
+        }
+
+    }
+}
