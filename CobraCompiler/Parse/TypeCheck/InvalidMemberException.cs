ccf163660840f1411c72d4e84308d692b12c54f8
Drew Cutchins <drew.cutch@gmail.com>
2020-03-19 12:30:39 -0400

    Create InvalidMemberException

---
+++ CobraCompiler/Parse/TypeCheck/InvalidMemberException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Parse.TypeCheck
+{
+    class InvalidMemberException : TypingException
+    {
+        public override bool isWarning => false;
+        public InvalidMemberException(string typeName, string memberName, int lineNumber) : base($"{typeName} does not have member {memberName}", lineNumber)
+        {
+
+        }
+    }
+}
