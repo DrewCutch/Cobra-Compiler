783c6637b5f51470d78b46b74d8e28d8f21b3b62
Drew Cutchins <drew.cutch@gmail.com>
2020-03-20 16:49:26 -0400

    Create ITypeGenerator interface

---
+++ CobraCompiler/Parse/TypeCheck/Types/ITypeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Parse.TypeCheck.Types
+{
+    /*
+     * Used for Generics generating Types not yet representable by Cobra Typing Features
+     */
+    interface ITypeGenerator
+    {
+        Type GetType(ModuleBuilder mb, params Type[] typeArgs);
+    }
+}
