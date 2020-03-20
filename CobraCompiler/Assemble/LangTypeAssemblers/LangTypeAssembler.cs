f00d78b81d9cd8df95a201024098e7870617ee0d
Drew Cutchins <drew.cutch@gmail.com>
2020-03-20 16:50:34 -0400

    Create abstract LangTypeAssemblers class

---
+++ CobraCompiler/Assemble/LangTypeAssemblers/LangTypeAssembler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Assemble.LangTypeAssemblers
+{
+    abstract class LangTypeAssembler
+    {
+        public abstract Type Assemble(ModuleBuilder mb, params Type[] typeParams);
+    }
+}
