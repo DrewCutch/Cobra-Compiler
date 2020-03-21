3340b510cdee4f473a5a938e26b2264d29371efd
Drew Cutchins <drew.cutch@gmail.com>
2020-03-21 12:00:19 -0400

    Create TypeDeclarationStatement

---
+++ CobraCompiler/Parse/Statements/TypeDeclarationStatement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Expressions;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Parse.Statements
+{
+    class TypeDeclarationStatement: Statement
+    {
+        public readonly Token Name;
+        public readonly TypeInitExpression Type;
+
+        public TypeDeclarationStatement(Token name, TypeInitExpression type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
