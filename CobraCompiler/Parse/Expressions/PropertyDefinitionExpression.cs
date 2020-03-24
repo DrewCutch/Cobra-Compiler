249328adf582f4b61970333c62958cc60c448888
Drew Cutchins <drew.cutch@gmail.com>
2020-03-23 21:39:08 -0400

    Create PropertyDefinitionExpression model

---
+++ CobraCompiler/Parse/Expressions/PropertyDefinitionExpression.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Parse.Expressions
+{
+    class PropertyDefinitionExpression: Expression
+    {
+        public readonly Token Identifier;
+        public readonly TypeInitExpression Type;
+
+        public PropertyDefinitionExpression(Token identifier, TypeInitExpression type)
+        {
+            Identifier = identifier;
+            Type = type;
+        }
+
+        public override T Accept<T>(IExpressionVisitor<T> expressionVisitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Accept<T>(IExpressionTraverser<T> expressionTraverser, T arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Accept<T, TU>(IExpressionVisitorWithContext<T, TU> expressionVisitor, TU arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
