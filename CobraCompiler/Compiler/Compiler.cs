4ee6191566a9ddd24355032c0173fd9133359876
Drew Cutchins <drew.cutch@gmail.com>
2020-03-27 20:48:24 -0400

    Remove Depreciated Run method from Compiler

--- CobraCompiler/Compiler/Compiler.cs
+++ CobraCompiler/Compiler/Compiler.cs
@@ -122,56 +122,6 @@ namespace CobraCompiler.Compiler
 
             assembler.SaveAssembly();
         }
-
-        public void Run()
-        {
-            FileReader fileReader = new FileReader(Options.FilePath);
-            Scanner scanner = new Scanner(fileReader, ErrorLogger);
-
-            IReadOnlyList<Token> tokens = scanner.GetTokens();
-
-            if (Options.Flags.HasFlag(CompilerFlags.PrintScan))
-            {
-                foreach (Token token in tokens)
-                {
-                    if (token.Type == TokenType.NewLine)
-                        Console.WriteLine(token);
-                    else
-                        Console.Write(token);
-                }
-                Console.WriteLine();
-            }
-
-            if (ErrorLogger.HasErrors)
-                return;
-
-            Parser parser = new Parser(tokens, ErrorLogger);
-            List<Statement> statements = parser.Parse();
-
-            if (Options.Flags.HasFlag(CompilerFlags.PrintParse))
-            {
-                AstPrinter astPrinter = new AstPrinter();
-                astPrinter.PrintStatements(statements);
-            }
-
-            if (ErrorLogger.HasErrors)
-                return;
-
-            TypeChecker typeChecker = new TypeChecker(ErrorLogger);
-
-            ModuleScope module = typeChecker.CreateModule(statements, "supportedcode");
-
-            if (ErrorLogger.HasErrors)
-                return;
-
-            Assembler assembler = new Assembler("CobraTest");
-            //assembler.AssembleModule(module);
-
-            if (ErrorLogger.HasErrors)
-                return;
-
-            assembler.SaveAssembly();
-        }
     }
 }
 
