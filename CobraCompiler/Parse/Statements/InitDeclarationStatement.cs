3f430a1bcae89ef67fc6216f925c6282ff09b53c
Drew Cutchins <drew.cutch@gmail.com>
2020-05-05 11:28:50 -0400

    Create InitDeclarationStatement

---
+++ CobraCompiler/Parse/Statements/InitDeclarationStatement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Expressions;
+using CobraCompiler.Scanning;
+
+namespace CobraCompiler.Parse.Statements
+{
+    class InitDeclarationStatement: FuncDeclarationStatement
+    {
+        public InitDeclarationStatement(Token keyWord, IReadOnlyList<ParamDeclarationStatement> @params, Statement body): base(keyWord, @params, null, body)
+        { }
+    }
+}
