de50bf915391880b66a36f0d93239c9cdf2205e5
Drew Cutchins <drew.cutch@gmail.com>
2020-05-05 11:49:11 -0400

    Create IntersectionAssembler

---
+++ CobraCompiler/Assemble/LangTypeAssemblers/IntersectionAssembler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Assemble.LangTypeAssemblers
+{
+    class IntersectionAssembler
+    {
+        private const TypeAttributes IntersectionTypeAttributes =
+            TypeAttributes.Public | TypeAttributes.SpecialName | TypeAttributes.Abstract | TypeAttributes.Interface;
+        public static Type Assemble(ModuleBuilder mb, params Type[] typeParams)
+        {
+            string name = typeParams.Aggregate("@Intersection", (current, typeParam) => current + $"_{typeParam.Name}");
+            TypeBuilder intersection = mb.DefineType(name, IntersectionTypeAttributes);
+            foreach (Type type in typeParams)
+                intersection.AddInterfaceImplementation(type);
+
+            intersection.CreateType();
+            return intersection;
+        }
+    }
+}
