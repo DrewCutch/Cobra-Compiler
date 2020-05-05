4ee7e5caee7dba96ffafc9d60c6ff2c501f439be
Drew Cutchins <drew.cutch@gmail.com>
2020-05-05 11:49:50 -0400

    Create ClassScope

---
+++ CobraCompiler/Parse/Scopes/ClassScope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.Statements;
+using CobraCompiler.Parse.TypeCheck.Types;
+
+namespace CobraCompiler.Parse.Scopes
+{
+    class ClassScope: Scope
+    {
+        public readonly ClassDeclarationStatement ClassDeclaration;
+        public readonly CobraType ThisType;
+
+        public ClassScope(Scope parentScope, ClassDeclarationStatement classDeclaration) : base(parentScope, classDeclaration.Body)
+        {
+            ClassDeclaration = classDeclaration;
+            ThisType = new CobraType("this", parentScope.GetType(classDeclaration.Type));
+            Declare("this", ThisType);
+        }
+
+        public override void Declare(string var, CobraType type)
+        {
+            if(var == "init" && type is CobraGenericInstance genericInstance && genericInstance.Base == DotNetCobraGeneric.FuncType)
+                Parent.Declare(ClassDeclaration.Name.Lexeme, type);
+
+
+            ThisType.DefineSymbol(var, type);
+            base.Declare(var, type);
+        }
+    }
+}
