55107043c5c1dc0e0f18c62ac1c4d800faddd8f8
Drew Cutchins <drew.cutch@gmail.com>
2020-07-29 15:38:17 -0400

    Add Type property to Expression
    I was stuck deciding how to solve the AST typing problem. I ended up just adding a nullable Type field to the abstract Expression class. While this is not "correct" it provides a simple solution that allows for me to continue work on the compiler while storing type information in the AST. This will probably be revisted in the future.

--- CobraCompiler/Parse/Expressions/Expression.cs
+++ CobraCompiler/Parse/Expressions/Expression.cs
@@ -1,9 +1,35 @@
 ﻿using CobraCompiler.Parse.PrettyPrint;
+using CobraCompiler.TypeCheck.Types;
 
 namespace CobraCompiler.Parse.Expressions
 {
     abstract class Expression
     {
+        private CobraType _type;
+        public CobraType Type
+        {
+            get
+            {
+                if(_type == null)
+                    throw new InvalidOperationException("The type has not been set for this expression");
+
+                return _type;
+            }
+
+            set
+            {
+                if (value == null)
+                    throw new InvalidOperationException("The type cannot be set to null");
+
+                _type = value;
+            }
+        }
+
+        protected Expression()
+        {
+            _type = null;
+        }
+
         public abstract T Accept<T>(IExpressionVisitor<T> expressionVisitor);
         public abstract void Accept<T>(IExpressionTraverser<T> expressionTraverser, T arg);
         public abstract T Accept<T, TU>(IExpressionVisitorWithContext<T, TU> expressionVisitor, TU arg);
