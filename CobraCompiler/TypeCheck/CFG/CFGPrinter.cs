4b4e3d240a4a71e5c8b5b9f4eb35f606e96bd745
Drew Cutchins <drew.cutch@gmail.com>
2020-10-06 15:08:53 -0700

    Create CFGPrinter
    CFGPrinter is used for debugging and visualizing the construction of Control Flow Graphs

---
+++ CobraCompiler/TypeCheck/CFG/CFGPrinter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.CFG;
+using CobraCompiler.Parse.PrettyPrint;
+using CobraCompiler.Parse.Statements;
+
+namespace CobraCompiler.TypeCheck.CFG
+{
+    class CFGPrinter
+    {
+        public void PrintCFG(CFGNode root)
+        {
+            List<CFGNode> allNodes = CFGNode.LinearNodes(root);
+
+            foreach (CFGNode node in allNodes)
+            {
+                PrintNode(node);
+            }
+        }
+
+        private void PrintNode(CFGNode node)
+        {
+            Console.WriteLine("\n-------------------------");
+            AstPrinter astPrinter = new AstPrinter();
+            astPrinter.PrintStatements(node.Statements);
+            Console.WriteLine("-------------------------\n");
+        }
+    }
+}
