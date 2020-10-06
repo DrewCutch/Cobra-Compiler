83094a55783ba7dfc1c0a147d727d4d942e2e3ee
Drew Cutchins <drew.cutch@gmail.com>
2020-10-06 15:10:10 -0700

    Create Mutability enum
    For now, mutability is an aspect of a symbol not a type. This could be changed in the future.

---
+++ CobraCompiler/TypeCheck/Symbols/Mutability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.TypeCheck.Symbols
+{
+    enum Mutability
+    {
+        Mutable,
+        AssignOnce,
+        ReadOnly,
+        CompileTimeConstant
+    }
+}
