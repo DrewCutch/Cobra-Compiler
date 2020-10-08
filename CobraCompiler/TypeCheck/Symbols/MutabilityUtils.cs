05619c8be01613e1f175489ebab2e85db3e6daa0
Drew Cutchins <drew.cutch@gmail.com>
2020-10-08 15:23:23 -0700

    Create MutabilityUtils with GetResultMutability method
    The GetResultMutability method returns the result mutuability of an operations consisting of operands of specified mutability.

---
+++ CobraCompiler/TypeCheck/Symbols/MutabilityUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.TypeCheck.Symbols
+{
+    class MutabilityUtils
+    {
+        public static Mutability GetResultMutability(params Mutability[] mutability)
+        {
+            if (mutability.All(m => m == Mutability.CompileTimeConstant || m == Mutability.CompileTimeConstantResult))
+                return Mutability.CompileTimeConstantResult;
+
+
+            return Mutability.Result;
+        }
+    }
+}
