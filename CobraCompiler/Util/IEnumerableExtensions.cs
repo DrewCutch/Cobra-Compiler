52f846dd7af03e296baab03f7797cd93c8ee2085
Drew Cutchins <drew.cutch@gmail.com>
2020-10-08 15:16:30 -0700

    Create IEnumerableExtensions class with OnlyOrDefault and Empty

---
+++ CobraCompiler/Util/IEnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobraCompiler.Util
+{
+    static class IEnumerableExtensions
+    {
+        public static T OnlyOrDefault<T>(this IEnumerable<T> enumerable)
+        {
+            IEnumerator<T> enumerator = enumerable.GetEnumerator();
+            try
+            {
+                if (!enumerator.MoveNext())
+                    return default;
+                
+                T only = enumerator.Current;
+
+                if (enumerator.MoveNext())
+                    return default;
+
+                return only;
+            }
+            finally
+            {
+                enumerator.Dispose();
+            }
+        }
+
+        public static bool Empty<T>(this IEnumerable<T> enumerable)
+        {
+            return !enumerable.Any();
+        }
+    }
+}
