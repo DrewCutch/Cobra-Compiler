b01e792f376613b41572544b1250ce4af3f6a73b
Drew Cutchins <drew.cutch@gmail.com>
2020-10-08 19:28:52 -0700

    Update VarDeclarationStatement with IsVal property

--- CobraCompiler/Parse/Statements/VarDeclarationStatement.cs
+++ CobraCompiler/Parse/Statements/VarDeclarationStatement.cs
@@ -9,12 +9,15 @@ namespace CobraCompiler.Parse.Statements
         public readonly Token Name;
         public readonly TypeInitExpression TypeInit;
         public readonly AssignExpression Assignment;
+        public readonly bool IsVal;
         public VarDeclarationStatement(Token keyWord, Token name, TypeInitExpression typeInit, AssignExpression assignment)
         {
             KeyWord = keyWord;
             Name = name;
             TypeInit = typeInit;
             Assignment = assignment;
+
+            IsVal = keyWord.Lexeme == "val";
         }
     }
 }
