132fd4ff676903f2aa77b4c33679be7d19b9c043
Drew Cutchins <drew.cutch@gmail.com>
2021-03-06 18:38:29 -0800

    Create IsAssigned function in ControlFlowCheck

---
+++ CobraCompiler/TypeCheck/CFG/ControlFlowCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CobraCompiler.Parse.CFG;
+using CobraCompiler.Parse.Scopes;
+using CobraCompiler.TypeCheck.Symbols;
+
+namespace CobraCompiler.TypeCheck.CFG
+{
+    class ControlFlowCheck
+    {
+        public static Func<CFGNode, bool> IsAssigned(Symbol symbol)
+        {
+            return (node) =>
+            {
+                bool isInit = node.Scope is FuncScope funcScope && funcScope.FuncDeclaration.Name.Lexeme == "init";
+
+                if (symbol.Mutability != Mutability.Mutable && node.Scope.Parent is ClassScope containingClass && !isInit)
+                    return true;
+
+                if (node.IsRoot && node.Scope.Parent.IsDefined(symbol.Lexeme) && !isInit)
+                    return true;
+
+                return node.Assignments.ContainsKey(symbol);
+            };
+        }
+    }
+}
